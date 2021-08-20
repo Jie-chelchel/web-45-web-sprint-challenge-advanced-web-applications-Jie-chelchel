f0b077d8126b88dafd0621df33f9585dbec122ba
Jie Zhang <zhangjiew@gmail.com>
2021-08-19 23:48:30 -0600

    fetched color

--- src/services/fetchColorService.js
+++ src/services/fetchColorService.js
@@ -1,7 +1,11 @@
-import axiosWithAuth from '../helpers/axiosWithAuth';
+import axiosWithAuth from "../helpers/axiosWithAuth";
 
 const fetchColorService = () => {
-    
-}
+  return axiosWithAuth().get("/colors");
+
+  // .catch((err) => {
+  //   return err.message;
+  // });
+};
 
 export default fetchColorService;
