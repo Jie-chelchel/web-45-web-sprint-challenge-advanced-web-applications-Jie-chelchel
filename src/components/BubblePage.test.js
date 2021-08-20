f899e1e3d743e2040f75b687f438f1bd49b8d30c
Jie Zhang <zhangjiew@gmail.com>
2021-08-20 15:39:56 -0600

    cannot get the bubblepage test done

--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -7,27 +7,27 @@ import BubblePage from "./BubblePage";
 test("Renders without errors", () => {
   render(<BubblePage />);
 });
-// jest.mock("../services/fetchColorService");
+jest.mock("../services/fetchColorService");
+//tried more than 100 times, I cannot get this one work :( 
 
 test("Renders appropriate number of colors passed in through mock", async () => {
   // Keep in mind that our service is called on mount for this component.
-  // fetchColorService.mockResolvedValueOnce({
-  //   data: [
-  //     {
-  //       code: { hex: "#6093ca" },
-  //       color: "blue",
-  //       id: 10,
-  //     },
-  //     {
-  //       code: { hex: "#000000" },
-  //       color: "black",
-  //       id: 9,
-  //     },
-  //   ],
-  // });
-  // render(<BubblePage />);
-  // const colors = await screen.findAllByTestId("color");
-  // expect(colors).toBeTruthy();
+  fetchColorService.mockResolvedValueOnce({
+    data: [
+      {
+        code: { hex: "#6093ca" },
+        color: "blue",
+        id: 10,
+      },
+      {
+        code: { hex: "#000000" },
+        color: "black",
+        id: 9,
+      },
+    ],
+  });
+  render(<BubblePage />);
+  const colors = await screen.findAllByTestId("color");
+  expect(colors).toBeTruthy();
 });
 
-//tried more than 100 times, I cannot get this one work :(
